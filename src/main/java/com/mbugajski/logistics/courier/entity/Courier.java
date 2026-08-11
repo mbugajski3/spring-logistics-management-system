0de83f423ebb552625e33572453b71d3be7bc962
Michał Bugajski <michabugajski2@gmail.com>
2026-08-11 22:43:01 +0200

    feat: add Courier entity

---
+++ src/main/java/com/mbugajski/logistics/courier/entity/Courier.java
@@ -0,0 +1,83 @@
+package com.mbugajski.logistics.courier.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "couriers")
+@Getter
+public class Courier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private boolean available;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    protected Courier() {
+    }
+
+    public Courier(String firstName, String lastName, String phoneNumber) {
+        if (firstName == null || firstName.isBlank()) {
+            throw new IllegalArgumentException("First name cannot be empty.");
+        }
+        this.firstName = firstName.trim();
+
+        if (lastName == null || lastName.isBlank()) {
+            throw new IllegalArgumentException("Last name cannot be empty.");
+        }
+        this.lastName = lastName.trim();
+
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            throw new IllegalArgumentException("Phone number cannot be empty.");
+        }
+        this.phoneNumber = phoneNumber.trim();
+
+        this.available = true;
+        this.active = true;
+    }
+
+    public void deactivate() {
+        if (!this.active || !this.available) {
+            throw new IllegalStateException("Only active courier can be deactivated.");
+        }
+        this.active = false;
+        this.available = false;
+
+    }
+
+    public void activate() {
+        if (this.active) {
+            throw new IllegalStateException("Only inactive courier can be activated.");
+        }
+        this.active = true;
+        this.available = true;
+    }
+
+    public void markAsBusy() {
+        if (!this.active || !this.available) {
+            throw new IllegalStateException("Courier must be active and available to mark as busy.");
+        }
+        this.available = false;
+    }
+
+    public void markAsAvailable() {
+        if (!this.active || this.available) {
+            throw new IllegalStateException("Courier must be active and busy to mark as available.");
+        }
+        this.available = true;
+    }
+}
